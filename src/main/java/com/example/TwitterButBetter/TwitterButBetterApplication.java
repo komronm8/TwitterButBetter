9ee6849e08c4baa0a397e88e09f7bc2b2ea65651
Komron <komron.ba@gmail.com>
2023-05-27 15:00:12 +0200

    initial commit

---
+++ src/main/java/com/example/TwitterButBetter/TwitterButBetterApplication.java
@@ -0,0 +1,13 @@
+package com.example.TwitterButBetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterButBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterButBetterApplication.class, args);
+	}
+
+}

a215d27ac03d750c7f55a02f868417f432264542
Cambriel <yamato12367@gmail.com>
2023-06-07 22:33:29 +0200

    added forgot password module and custom validators

---
+++ TwitterFrontend/src/app/forgot-password/customValidators.ts
@@ -0,0 +1,29 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export function validPasswordStrength(): ValidatorFn {
+  return (control:AbstractControl) : ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+          return null;
+      }
+      const hasUpperCase = /[A-Z]+/.test(value);
+      const hasLowerCase = /[a-z]+/.test(value);
+      const hasNumeric = /[0-9]+/.test(value);
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+      return !passwordValid ? {passwordStrength:true}: null;
+  }
+}
+
+export function confirmPassword(password: String): ValidatorFn {
+  return (control:AbstractControl) : ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+          return null;
+      }
+      const hasUpperCase = /[A-Z]+/.test(value);
+      const hasLowerCase = /[a-z]+/.test(value);
+      const hasNumeric = /[0-9]+/.test(value);
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+      return !passwordValid ? {notConfirmed: true}: null;
+  }
+}
--- TwitterFrontend/src/app/forgot-password/forgot-password.component.ts
+++ TwitterFrontend/src/app/forgot-password/forgot-password.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { validPasswordStrength, confirmPassword } from './customValidators';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class ForgotPasswordComponent {
 
+  //FormGroup holding password and confirmPassword
+  matchPw: FormGroup;
+  errors = {
+    password: null,
+    confirmPassword: null
+  };
+
+  tooltipText = [
+    "Your password must have:",
+    " ",
+    "minimum of 8 characters",
+    "1 upper case letter",
+    "1 lower case letter",
+    "1 numeric letter"
+  ].join("\n");
+
+  hidePassword = true;
+  hideRepeatPassword = true;
+  password: string = '';
+
+  constructor(private fb: FormBuilder){
+    this.matchPw = this.fb.group({
+      password: ['', {
+        validators: [ Validators.required, Validators.minLength(8), validPasswordStrength() ],
+        asyncValidators: [],
+        updateOn: 'change'
+      }],
+      confirmPassword: ['', {
+        validators: [ Validators.required, confirmPassword(this.password) ],
+        asyncValidators: [],
+        updateOn: 'change'
+      }]
+    });
+  }
+
+  passwordErrors(control: AbstractControl) {
+    return control.hasError('required') || control.hasError('minength') || control.hasError('passwordStrength');
+  }
+  confirmErrors(control: AbstractControl){
+    return control.hasError('notConfirmed');
+  }
+
+  onSubmit(){
+    //TODO: Add HTTP-Request
+  }
 }

692bb421ed9f89d04959e0e879b983c23a71423f
Maximilian Dettmer <dettmer.maximilian@gmail.com>
2023-06-09 18:13:38 +0200

    added http request for active session request

--- TwitterFrontend/src/app/dashboard/dashboard.component.ts
+++ TwitterFrontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  session: number = 0;
+  sessionActive: boolean = false;
+
+  constructor(private http: HttpService){
+
+  }
 
   newPost(){
     //TODO: open new tab to github-page 
@@ -14,5 +21,17 @@ export class DashboardComponent {
 
   openMenu(){
     //TODO: make sidenav open over main content or minimize with only icons seen
+  }
+
+  ngOnInit(){
+    this.http.isSessionActive(this.session).subscribe(
+      (response: {message: string}) => {
+        if(response.message.startsWith('true')) {
+          this.sessionActive = true;
+        } else {
+          this.sessionActive = false;
+        }
+      }
+    );
   }
 }

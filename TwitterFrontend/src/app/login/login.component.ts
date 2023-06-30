4460776d21358523ebde195fdc8d35c43a4d4d07
Assanali Uzakov <assanali.uzakov@interhome.group>
2023-06-30 21:05:29 +0200

    Added registration

--- TwitterFrontend/src/app/login/login.component.ts
+++ TwitterFrontend/src/app/login/login.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  login: Boolean = false
+  
+  handleSubmit() {
+    const username = document.querySelector('input[name=username]') as HTMLTextAreaElement
+    const password = document.querySelector('input[name=password]') as HTMLTextAreaElement
+    console.log(username.value)
+    console.log(password.value)
+  }
 }
--- TwitterFrontend/src/app/tweet/tweet.component.ts
+++ TwitterFrontend/src/app/tweet/tweet.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-tweet',
@@ -7,6 +7,11 @@ import { Component, Input } from '@angular/core';
 })
 export class TweetComponent {
   @Input() tweet: any;
+  @Input() isAuthor: Boolean | undefined;
+  @Output() edit: EventEmitter<any> = new EventEmitter();
+  editPost() {
+    this.edit.emit();
+  }
   constructor() {
   }
 }

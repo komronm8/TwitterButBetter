785b653cff0e79d2bf4b20a230f34f91768378e3
Assanali Uzakov <assanali.uzakov@interhome.group>
2023-07-02 16:29:08 +0200

    added authentication in frontend

--- TwitterFrontend/src/app/login/login.component.ts
+++ TwitterFrontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
+
   login: Boolean = false
   
   handleSubmit() {
-    const username = document.querySelector('input[name=username]') as HTMLTextAreaElement
+    const name = document.querySelector('input[name=name]') as HTMLTextAreaElement
+    const email = document.querySelector('input[name=email]') as HTMLTextAreaElement
     const password = document.querySelector('input[name=password]') as HTMLTextAreaElement
-    console.log(username.value)
-    console.log(password.value)
+    type data = {
+      email: String,
+      password: String,
+      name?: String
+    }
+    const data:data = {
+      email: email.value,
+      password: password.value
+    }
+    if (!this.login) {
+      data.name = name.value
+    }
+    console.log(data)
+    async function fetchRequest(url: string, data: object) {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      return response.json();
+    }
+    
+    const api = this.login ? 'http://localhost:8080/api/v1/auth/authenticate' :'http://localhost:8080/api/v1/auth/register'
+    fetchRequest(api, data)
+      .then((data) => {
+        localStorage.setItem('jwt', data.token);
+        console.log(data)
+        this.router.navigate(['/home']);
+      })
+      .catch((error) => {
+        alert(error)
+      });
+    
   }
 }

b617866f31a325cc1a756b004d352df9aa1cf9de
Komron <komron.ba@gmail.com>
2023-07-02 16:25:30 +0200

    Minor changes done

--- twitterbackend/src/main/java/com/example/twitterbackend/Post/PostService.java
+++ twitterbackend/src/main/java/com/example/twitterbackend/Post/PostService.java
@@ -1,11 +1,13 @@
 package com.example.twitterbackend.Post;
 
+import com.example.twitterbackend.User.Role;
 import com.example.twitterbackend.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class PostService {
@@ -36,20 +38,29 @@ public class PostService {
         return postRepository.findUserPosts(userId);
     }
 
-    public void deletePost(Integer postId) {
+    public void deletePost(Integer postId, User user) {
         boolean exists = postRepository.existsById(postId);
         if(!exists){
             throw new IllegalStateException("Post with id " + postId + " does not exist");
         }
+        Post post = postRepository.getReferenceById(postId);
+        if(user.getRole() != Role.ADMIN){
+            if(!Objects.equals(post.getUser().getId(), user.getId())){
+                throw new IllegalStateException("User does not have access");
+            }
+        }
         postRepository.deleteById(postId);
     }
 
-    public void updatePost(Post post) {
+    public void updatePost(Post post, User user) {
         boolean exists = postRepository.existsById(post.getId());
         if(!exists){
             throw new IllegalStateException("Post with id " + post.getId() + " does not exist");
         }
         Post currentPost = postRepository.getReferenceById(post.getId());
+        if(!Objects.equals(currentPost.getUser().getId(), user.getId())){
+            throw new IllegalStateException("User does not have access");
+        }
         currentPost.setText(post.getText());
         postRepository.save(currentPost);
     }

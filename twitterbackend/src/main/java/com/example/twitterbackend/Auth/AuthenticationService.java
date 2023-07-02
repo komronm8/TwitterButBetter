c4de9c677a477276f82c9a052aa2bf10a05ce8e7
Komron <komron.ba@gmail.com>
2023-07-02 15:34:53 +0200

    Minor changes done

--- twitterbackend/src/main/java/com/example/twitterbackend/Auth/AuthenticationService.java
+++ twitterbackend/src/main/java/com/example/twitterbackend/Auth/AuthenticationService.java
@@ -21,7 +21,7 @@ public class AuthenticationService {
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
-                .name(request.getFirstname() + " " + request.getLastname())
+                .name(request.getName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
